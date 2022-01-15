990bc192694f0f8f50621f9c1cd908abcba19cc1
Muhammad Khan <UmarP@Umars-MacBook-Pro.local>
2022-01-15 13:18:50 -0500

    Add JOIN statemnts to join tables

--- 1_queries/currently_enrolled_students.sql
+++ 1_queries/currently_enrolled_students.sql
@@ -2,3 +2,9 @@ SELECT name, id, email, cohort_id
 FROM students
 WHERE end_date IS NULL
 ORDER BY cohort_id
+
+
+SELECT student_name, student_start_date, cohort_start_date, cohort_name
+FROM students 
+JOIN cohort_start_date ON student_start_date
+ORDER BY cohort_start_date ASC
